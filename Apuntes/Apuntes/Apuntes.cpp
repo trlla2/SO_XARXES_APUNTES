19bdbd1cb83dcfa9c036d5b0576ffd754f403471
trlla2 <116338442+trlla2@users.noreply.github.com>
2025-09-23 17:45:30 +0200

    lamba lmao

--- Apuntes/Apuntes/Apuntes.cpp
+++ Apuntes/Apuntes/Apuntes.cpp
@@ -3,22 +3,30 @@
 #include "Utils/ConsoleControl.h"
 
 #include <functional>
+#include <list>
 
-void TestLambasMolonas(std::function<int(int, int)> funcionMolona)
+typedef std::function<int(int, int)> SumaFunction;
+typedef std::list<std::list<int>> ListDeList; // alias del tipo de variable/function
+
+void TestLambasMolonas(SumaFunction funcionMolona)
 {
 	std::cout << "Voy a ejecutar una funciion aaaaaaaaaaa" << std::endl;
 	
 	int number = funcionMolona(5 , 7);
 	
-	std::cout << "ya e ejecutado la function" << std::endl;
+	std::cout << "ya e ejecutado la function y devuelve: " << number << std::endl;
 }
 
 int main()
 {
-	std::function<int(int,int)> funcionKHaceCosasPeroS1Variable = []( int a, int b) {
+	int c = 30;
+
+	ListDeList listDeList;
+
+	std::function<int(int,int)> funcionKHaceCosasPeroS1Variable = [c]( int a, int b) {
 
 		std::cout << "Esto no se como pero funca" << std::endl;
-		return a + b;
+		return a + b + c;
 		};
 
 	TestLambasMolonas(funcionKHaceCosasPeroS1Variable);
