fdb991b31fbe9787f32a9ccc4e98cd73838e6099
trlla2 <116338442+trlla2@users.noreply.github.com>
2025-09-23 17:34:11 +0200

    principio templates

--- Apuntes/Apuntes/Apuntes.cpp
+++ Apuntes/Apuntes/Apuntes.cpp
@@ -4,19 +4,21 @@
 
 #include <functional>
 
-void TestLambasMolonas(std::function<void()> funcionMolona)
+void TestLambasMolonas(std::function<int(int, int)> funcionMolona)
 {
 	std::cout << "Voy a ejecutar una funciion aaaaaaaaaaa" << std::endl;
 	
-	funcionMolona();
+	int number = funcionMolona(5 , 7);
 	
 	std::cout << "ya e ejecutado la function" << std::endl;
 }
 
 int main()
 {
-	std::function<void()> funcionKHaceCosasPeroS1Variable = []() {
+	std::function<int(int,int)> funcionKHaceCosasPeroS1Variable = []( int a, int b) {
+
 		std::cout << "Esto no se como pero funca" << std::endl;
+		return a + b;
 		};
 
 	TestLambasMolonas(funcionKHaceCosasPeroS1Variable);
